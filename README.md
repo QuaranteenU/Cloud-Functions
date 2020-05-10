26fff01d5c41260a687ddc2be0dc0f890fd9c9b4
Rudhra Raveendran <rudytheninja@gmail.com>
2020-05-10 17:10:23 -0400

    Ready for deploy

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Cloud-Functions
-Repo for our GCP cloud functions
+# Cloud Functions
+Repo for QU's GCP cloud functions.
+
+## checkEmail
+Describes an HTTP Cloud Function, receives POST request with body like so:
+```
+{
+  "email": "someone@gmail.com",
+  "event": "QU" // also accepts "QUA"
+}
+```
+Depending on the value of "event", the function will check whether the email is signed up/RSVP'd in either the QU or QUA spreadsheets, returning a response like so:
+```
+{
+  signedUp,   // whether the email is signed up
+  signUpInfo, // if the above is true, what info we have
+  rsvpd,      // whether the email rsvp'd
+  rsvpdNew    // for QU only, whether the email rsvp'd through the new form
+}
+```
+Requires the existence of `api_key.json` and `credentials.json` (service account credentials) in the `functions` folder. Create them in the GCP Console: APIS & Services -> Credentials. Make sure the API Key has access to the Google Sheets API. Last step is granting access to the email of the service account to the Google Sheets you'd like to read from (click Share on the sheets and add the service account with Read permissions).
--- functions/checkEmail.js
+++ functions/checkEmail.js
@@ -1,8 +1,8 @@
 const { google } = require("googleapis");
 
 exports.handler = async (req, res) => {
+  // allow access from other origins
   res.set("Access-Control-Allow-Origin", "*");
-
   if (req.method === "OPTIONS") {
     res.set("Access-Control-Allow-Methods", "POST");
     res.set("Access-Control-Allow-Headers", "Content-Type");
@@ -10,35 +10,95 @@ exports.handler = async (req, res) => {
     res.status(204).send("");
   }
 
+  // load our credentials
   const jwt = getJwt();
   const apiKey = getApiKey();
 
-  const signUpSheetId = "13axut8BiM4KwYqUI7IX1xLRXPttZKbbQrnrMQVVQM5k";
-  let signUps = await getSheetData(jwt, apiKey, signUpSheetId, "B2:F");
-  signUps = signUps.data.values
-    .filter(v => v.length === 5)
-    .map(v => {
-      return {
-        name: `${v[0]} ${v[1]}`,
-        email: v[2],
-        timezone: v[3],
-        role: v[4],
-      };
-    });
-  const signUpEmails = signUps.map(s => s.email);
-  const signedUp = signUpEmails.includes(req.body.email);
+  // make sure the request is properly formatted
+  if (!req.body.hasOwnProperty('email')) {
+    res.status(400).send(`Request body requires 'email' property!`);
+    return;
+  }
+  if (!req.body.hasOwnProperty('event')) {
+    res.status(400).send(`Request body requires 'event' property!`);
+    return;
+  }
 
-  const rsvpSheetId = "1GGdJ8K_MMXoflcuNDslYw75_jzd6F14MIm62hFIYK5k";
-  let rsvps = await getSheetData(jwt, apiKey, rsvpSheetId, "B2:B");
-  rsvps = rsvps.data.values.filter(v => v.length === 1).map(v => v[0]);
-  const rsvpd = rsvps.includes(req.body.email);
-  res.send({
-    signedUp,
-    signUpInfo: signUps.find(s => s.email === req.body.email),
-    rsvpd,
-  });
+  // notice the braces in the case statements, they're required to scope the const variables. otherwise the engine will complain that they've already been defined
+  switch (req.body.event) {
+    case 'QU': {
+      // check if the user has signed up
+      const signUpSheetId = "13axut8BiM4KwYqUI7IX1xLRXPttZKbbQrnrMQVVQM5k";
+      let signUps = await getSheetData(jwt, apiKey, signUpSheetId, "B2:F");
+      signUps = signUps.data.values ? signUps.data.values
+        .filter(v => v.length === 5)
+        .map(v => {
+          return {
+            name: `${v[0]} ${v[1]}`,
+            email: v[2],
+            timezone: v[3],
+            role: v[4],
+          };
+        }) : [];
+      const signUpEmails = signUps.map(s => s.email);
+      const signedUp = signUpEmails.includes(req.body.email);
+
+      // check if the user has rsvp'd
+      const rsvpSheetId = "1GGdJ8K_MMXoflcuNDslYw75_jzd6F14MIm62hFIYK5k";
+      let rsvps = await getSheetData(jwt, apiKey, rsvpSheetId, "B2:B");
+      rsvps = rsvps.data.values ? rsvps.data.values.filter(v => v.length === 1).map(v => v[0]) : [];
+      const rsvpd = rsvps.includes(req.body.email);
+
+      // check if the user has rsvp'd in the new form (QU only)
+      const newRSVPSheetId = "1NqC5gnSiyBNDmPYg28mY7EbVojmYYOypdh5TzXys4WE";
+      let newRSVPs = await getSheetData(jwt, apiKey, newRSVPSheetId, "B2:B");
+      newRSVPs = newRSVPs.data.values ? newRSVPs.data.values.filter(v => v.length === 1).map(v => v[0]) : [];
+      const rsvpdNew = newRSVPs.includes(req.body.email);
+
+      res.send({
+        signedUp,
+        signUpInfo: signUps.find(s => s.email === req.body.email),
+        rsvpd,
+        rsvpdNew,
+      });
+      } break;
+    case 'QUA': {
+      // check if the user has signed up
+      const signUpSheetId = "1jq1U5FoR47OvSKtZkQ_E2euWYWOm5kbvopGfU3wY39E";
+      let signUps = await getSheetData(jwt, apiKey, signUpSheetId, "B2:F");
+      signUps = signUps.data.values ? signUps.data.values
+        .filter(v => v.length === 5)
+        .map(v => {
+          return {
+            name: `${v[0]} ${v[1]}`,
+            email: v[2],
+            timezone: v[3],
+            role: v[4],
+          };
+        }) : [];
+      const signUpEmails = signUps.map(s => s.email);
+      const signedUp = signUpEmails.includes(req.body.email);
+
+      // check if the user has rsvp'd
+      const rsvpSheetId = "1A3wrDiaC7SICy8fs4S8qNvzpx28UsfNn8NLfpOmPtD8";
+      let rsvps = await getSheetData(jwt, apiKey, rsvpSheetId, "B2:B");
+      rsvps = rsvps.data.values ? rsvps.data.values.filter(v => v.length === 1).map(v => v[0]) : [];
+      const rsvpd = rsvps.includes(req.body.email);
+
+      res.send({
+        signedUp,
+        signUpInfo: signUps.find(s => s.email === req.body.email),
+        rsvpd,
+      });
+      } break;
+    default:
+      // catch bad request
+      res.status(400).send(`Unknown value for 'event': ${req.body.event}`);
+      break;
+  }
 };
 
+// retrieve JWT to read the spreadsheets
 const getJwt = () => {
   const credentials = require("./credentials.json");
   return new google.auth.JWT(
@@ -49,11 +109,13 @@ const getJwt = () => {
   );
 };
 
+// retrieve API key. unused as of yet as we need it for writing, not for reading
 const getApiKey = () => {
   const apiKeyFile = require("./api_key.json");
   return apiKeyFile.key;
 };
 
+// load the given spreadsheet by id and range, then return the result
 const getSheetData = async (jwt, apiKey, spreadsheetId, range) => {
   const sheets = google.sheets({ version: "v4" });
   const res = await sheets.spreadsheets.values.get({
